4c9b41f8d71d4c1d54afb117f8dca79f92255d30
Jadir Hervias <jadirhervias@gmail.com>
2020-04-23 19:39:59 -0500

    SplashActivity

---
+++ laboratorio_3/app/src/main/java/com/tecsup/patitas/veteriapp/activities/HomeActivity.java
@@ -0,0 +1,16 @@
+package com.tecsup.patitas.veteriapp.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.tecsup.patitas.veteriapp.R;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
